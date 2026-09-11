775ccae40d98d0469949c970545eb2b244c6edf8
Zeta <79603396+zetachess@users.noreply.github.com>
2026-09-11 14:29:11 +0200

    Initial Nextra chess manual

---
+++ app/[[...mdxPath]]/page.jsx
@@ -0,0 +1,17 @@
+import { generateStaticParamsFor, importPage } from 'nextra/pages'
+import { useMDXComponents as getMDXComponents } from '../../mdx-components'
+
+export const generateStaticParams = generateStaticParamsFor('mdxPath')
+
+export async function generateMetadata({ params }) {
+  const { metadata } = await importPage((await params).mdxPath)
+  return metadata
+}
+
+const Wrapper = getMDXComponents().wrapper
+
+export default async function Page(props) {
+  const params = await props.params
+  const { default: MDXContent, toc, metadata, sourceCode } = await importPage(params.mdxPath)
+  return <Wrapper toc={toc} metadata={metadata} sourceCode={sourceCode}><MDXContent {...props} params={params} /></Wrapper>
+}
